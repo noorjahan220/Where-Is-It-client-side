80c5dcf23b8794e6a0a87a0e7baf066b9e3de51e
noorjahan220 <noorjahanmeem220@gmail.com>
2024-12-25 00:39:38 +0600

    Fetch and display 6 most recent Find & Lost items, sorted by date

--- src/pages/Home/LatesItems.jsx
+++ src/pages/Home/LatesItems.jsx
@@ -8,15 +8,25 @@ const LatesItems = () => {
     useEffect(()=>{
         fetch('https://b10a11-server-side-noorjahan220.vercel.app/items')
         .then(res => res.json())
-        .then(data => setItems(data))
+        .then(data => {
+            const sortedItems = data.sort((a,b)=> new Date(b.date)-new Date(a.date));
+
+            const latestItems = sortedItems.slice(0,6);
+            setItems(latestItems);
+        })
+        .catch((error) => console.error('Error fetching items:', error));
     },[])
     return (
         <div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-            {
-                items.map(item => <ItemsCard key={item._id} item={item} />)
-            }
-        </div>
+             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {items.length > 0 ? (
+                    items.map((item) => <ItemsCard key={item._id} item={item} />)
+                ) : (
+                    <p className="text-center text-gray-500 col-span-full">
+                        No items available.
+                    </p>
+                )}
+            </div>
         <Link to="/allItems"><button>See More</button></Link>
         </div>
     );
